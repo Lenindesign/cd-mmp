207949d51215c153299361201e3d22285d4784dd
Lenindesign <Lenindesign@users.noreply.github.com>
2026-04-11 14:59:33 +0000

    Deploy Storybook - 9456e95d8e5e7a2a54a0d36037390e348f53efb2 9456e95d8e5e7a2a54a0d36037390e348f53efb2

---
+++ assets/formatter-QJ4M4OGQ-B_bNDk4z.js
@@ -0,0 +1,1 @@
+import{d as m,r as o}from"./blocks-CBm2bXKj.js";import{f as t}from"./iframe-C2SmGhXj.js";import"./preload-helper-PPVm8Dsz.js";import"./index-D7ha6_vD.js";import"./index-CSCS4Akp.js";var e=t(o(),1),_=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{_ as formatter};
