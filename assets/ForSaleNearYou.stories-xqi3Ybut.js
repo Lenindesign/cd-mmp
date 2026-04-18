5c0268de924acce6d4d78a18de78f01a17f8984e
Lenindesign <Lenindesign@users.noreply.github.com>
2026-04-18 22:19:13 +0000

    Deploy Storybook - 66ba13637870fdcf5657bd2e325cb83616241583 66ba13637870fdcf5657bd2e325cb83616241583

---
+++ assets/ForSaleNearYou.stories-xqi3Ybut.js
@@ -0,0 +1,32 @@
+import{F as t}from"./ForSaleNearYou-Dn8xpcpJ.js";import"./iframe-D7JhMSnj.js";import"./preload-helper-PPVm8Dsz.js";import"./listingsService-Bt48tBmc.js";import"./index-B-JoA3P0.js";import"./OptimizedImage-CQiroX4b.js";import"./Button-BgcxwTFZ.js";import"./map-pin-CpAldNDp.js";import"./createLucideIcon-CKJmH7vD.js";import"./chevron-left-DDa_bVwY.js";import"./bookmark-ClNJn61V.js";import"./gauge-B6kIQ_iz.js";import"./calendar-CpSYFTrM.js";import"./phone-mwQcpKMf.js";import"./navigation-DTq3C3LI.js";import"./chevron-right-pMmqbTW9.js";const F={title:"Organisms/ForSaleNearYou",component:t,parameters:{layout:"padded",docs:{description:{component:"Carousel of vehicles for sale near the user location."}}},tags:["autodocs"]},e={args:{vehicleName:"Toyota Camry",make:"Toyota",model:"Camry"}},o={args:{vehicleName:"Honda CR-V",make:"Honda",model:"CR-V",bodyStyle:"SUV",maxPrice:4e4,location:"Los Angeles, CA"}},a={args:{title:"Similar Vehicles Near You",make:"Toyota",bodyStyle:"Sedan",location:"New York, NY"}},r={args:{vehicleName:"Ford F-150",make:"Ford",model:"F-150",bodyStyle:"Truck",maxPrice:6e4,location:"Dallas, TX"}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    vehicleName: 'Toyota Camry',
+    make: 'Toyota',
+    model: 'Camry'
+  }
+}`,...e.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    vehicleName: 'Honda CR-V',
+    make: 'Honda',
+    model: 'CR-V',
+    bodyStyle: 'SUV',
+    maxPrice: 40000,
+    location: 'Los Angeles, CA'
+  }
+}`,...o.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    title: 'Similar Vehicles Near You',
+    make: 'Toyota',
+    bodyStyle: 'Sedan',
+    location: 'New York, NY'
+  }
+}`,...a.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    vehicleName: 'Ford F-150',
+    make: 'Ford',
+    model: 'F-150',
+    bodyStyle: 'Truck',
+    maxPrice: 60000,
+    location: 'Dallas, TX'
+  }
+}`,...r.parameters?.docs?.source}}};const h=["Default","SUV","CustomTitle","Truck"];export{a as CustomTitle,e as Default,o as SUV,r as Truck,h as __namedExportsOrder,F as default};
--- assets/formatter-QJ4M4OGQ-BdvE_uuL.js
+++
@@ -1,1 +0,0 @@
-import{d as m,r as o}from"./blocks-CbLrgh7c.js";import{f as t}from"./iframe-B_hBYtrn.js";import"./preload-helper-PPVm8Dsz.js";import"./index-C-F_QrHI.js";import"./index-CTh3Tthw.js";var e=t(o(),1),_=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{_ as formatter};
