f236cbc194af3ecf1af014e56f721a4f7bf1fcc9
Lenindesign <Lenindesign@users.noreply.github.com>
2026-04-29 13:44:30 +0000

    Deploy Storybook - de0f272a048f1d10d2192bb5ecc3619de8562bac de0f272a048f1d10d2192bb5ecc3619de8562bac

---
+++ assets/formatter-QJ4M4OGQ-ArZVVUzk.js
@@ -0,0 +1,1 @@
+import{d as m,r as o}from"./blocks-BDyuBwAg.js";import{f as t}from"./iframe-CBmx7sW-.js";import"./preload-helper-PPVm8Dsz.js";import"./index-CT5pNhSb.js";import"./index-D_cRDRd5.js";var e=t(o(),1),_=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{_ as formatter};
